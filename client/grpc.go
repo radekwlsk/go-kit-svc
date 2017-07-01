fc353fa713a4c2fded9462f36102be606b695471
afrometal <radek.kwlsk@gmail.com>
2017-07-02 00:35:48 +0200

    grpc client

---
+++ client/grpc.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"../stringsvc"
+	"../stringsvc/proto"
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+	"google.golang.org/grpc"
+)
+
+// Return new stringgrpc service
+func New(conn *grpc.ClientConn) stringsvc.StringService {
+	var titleCaseEndpoint = grpctransport.NewClient(
+		conn, "proto.String", "TitleCase",
+		stringsvc.EncodeGRPCTitleCaseRequest,
+		stringsvc.DecodeGRPCTitleCaseResponse,
+		proto.TitleCaseResponse{},
+	).Endpoint()
+
+	var removeWhitespaceEndpoint = grpctransport.NewClient(
+		conn, "proto.String", "RemoveWhitespace",
+		stringsvc.EncodeGRPCRemoveWhitespaceRequest,
+		stringsvc.DecodeGRPCRemoveWhitespaceResponse,
+		proto.RemoveWhitespaceResponse{},
+	).Endpoint()
+
+	var countEndpoint = grpctransport.NewClient(
+		conn, "proto.String", "Count",
+		stringsvc.EncodeGRPCCountRequest,
+		stringsvc.DecodeGRPCCountResponse,
+		proto.CountResponse{},
+	).Endpoint()
+
+	return stringsvc.Endpoints{
+		TitleCaseEndpoint:        titleCaseEndpoint,
+		RemoveWhitespaceEndpoint: removeWhitespaceEndpoint,
+		CountEndpoint:            countEndpoint,
+	}
+}
