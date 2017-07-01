3750053dc76c269a3c8f60b6fe699e5c6fe179f5
afrometal <radek.kwlsk@gmail.com>
2017-07-02 00:35:28 +0200

    grpc client

---
+++ cmd/client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	grpcClient "../client"
+	"../stringsvc"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var (
+		grpcAddr = flag.String("addr", ":8081",
+			"gRPC address")
+	)
+	flag.Parse()
+	ctx := context.Background()
+	conn, err := grpc.Dial(*grpcAddr, grpc.WithInsecure(),
+		grpc.WithTimeout(1*time.Second))
+
+	if err != nil {
+		log.Fatalln("gRPC dial:", err)
+	}
+	defer conn.Close()
+
+	stringService := grpcClient.New(conn)
+	args := flag.Args()
+	var cmd string
+
+	for len(args) > 0 {
+		cmd, args = pop(args)
+		switch cmd {
+		case "tc":
+			var s string
+			s, args = pop(args)
+			titleCase(ctx, stringService, s)
+		case "rw":
+			var s string
+			s, args = pop(args)
+			removeWhitespace(ctx, stringService, s)
+		case "c":
+			var s string
+			s, args = pop(args)
+			count(ctx, stringService, s)
+		default:
+			log.Fatalln("unknown command", cmd)
+		}
+	}
+}
+func count(ctx context.Context, service stringsvc.StringService, s string) {
+	fmt.Println(service.Count(ctx, s))
+}
+func removeWhitespace(ctx context.Context, service stringsvc.StringService, s string) {
+	output, err := service.RemoveWhitespace(ctx, s)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	fmt.Println(output)
+}
+func titleCase(ctx context.Context, service stringsvc.StringService, s string) {
+	output, err := service.TitleCase(ctx, s)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	fmt.Println(output)
+}
+
+// parse command line argument one by one
+func pop(s []string) (string, []string) {
+	if len(s) == 0 {
+		return "", s
+	}
+	return s[0], s[1:]
+}
